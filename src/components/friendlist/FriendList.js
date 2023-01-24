e14e2b2bfe885c0b2042d23bb77435db9e8ed66b
Iryna Grytsaenko <iryna.grytsaenko@gmail.com>
2023-01-24 23:07:02 +0200

    Adds css module for Profile component

--- src/components/friendlist/FriendList.js
+++ src/components/friendlist/FriendList.js
@@ -6,10 +6,10 @@ export default function FriendList({ friends }) {
       {friends.map(friend => {
         return (
           <FriendListItem
+            key={friend.id}
             avatar={friend.avatar}
             name={friend.name}
             isOnline={friend.isOnline}
-            key={friend.id}
             id={friend.id}
           />
         );
